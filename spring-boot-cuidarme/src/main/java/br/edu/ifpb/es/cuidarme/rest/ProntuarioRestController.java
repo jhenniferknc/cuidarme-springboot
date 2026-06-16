61e501d63723fc2d21904bb3a8d84c7b07a3d4b4
Emanuelle Guedes <manugguedes1701@gmail.com>
2026-06-16 14:47:10 -0300

    feat: adicionando endpoint de paginacao

--- spring-boot-cuidarme/src/main/java/br/edu/ifpb/es/cuidarme/rest/ProntuarioRestController.java
+++ spring-boot-cuidarme/src/main/java/br/edu/ifpb/es/cuidarme/rest/ProntuarioRestController.java
@@ -8,18 +8,14 @@ import br.edu.ifpb.es.cuidarme.exception.SistemaException;
 import br.edu.ifpb.es.cuidarme.mapper.ProntuarioMapper;
 import br.edu.ifpb.es.cuidarme.model.Prontuario;
 import br.edu.ifpb.es.cuidarme.rest.dto.Prontuario.ProntuarioResponseDTO;
+import br.edu.ifpb.es.cuidarme.rest.dto.Prontuario.ProntuarioBuscarDTO;
 import br.edu.ifpb.es.cuidarme.rest.dto.Prontuario.ProntuarioSalvarRequestDTO;
 import br.edu.ifpb.es.cuidarme.service.ProntuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
 
@@ -80,5 +76,22 @@ public class ProntuarioRestController implements ProntuarioRestControllerApi {
             throw new SistemaException("Prontuário com o ID " + lookupId + " não foi encontrado.");
         }
         return opt.get();
+    }
+
+    @Override
+    @GetMapping("/buscar-paginado")
+    public ResponseEntity<Page<ProntuarioResponseDTO>> buscarPaginado(
+            @RequestParam(defaultValue = "0", required = false) int numeroPagina,
+            @RequestParam(defaultValue = "10", required = false) int tamanhoPagina) {
+
+        ProntuarioBuscarDTO buscarDTO = new ProntuarioBuscarDTO();
+        buscarDTO.setNumeroPagina(numeroPagina);
+        buscarDTO.setTamanhoPagina(tamanhoPagina);
+
+        Page<Prontuario> pagina = prontuarioService.buscar(buscarDTO);
+
+        Page<ProntuarioResponseDTO> resultado = pagina.map(prontuarioMapper::from);
+
+        return new ResponseEntity<>(resultado, HttpStatus.OK);
     }
 }
